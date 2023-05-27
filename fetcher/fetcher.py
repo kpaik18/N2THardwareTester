fe0ae70769c98dc6126f4f890af58af8ca2e3266
Koba.Paikidze <koba.paikidze@ol.ge>
2023-05-27 13:32:00 +0400

    student submission download works

--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -22,14 +22,11 @@ class ClassroomFetcher:
         creds = self._auth()
         service = build("classroom", "v1", credentials=creds)
         course = self._get_course_by_code(service, course_code)
-        course_student_ids = self._get_course_student_ids(service, course)
+        course_students = self._get_course_students(service, course)
         coursework = self._get_coursework_by_course_and_code(
             service, course, coursework_code
         )
-        # self._download_student_submissions(
-        #     service, course_student_ids, course, coursework
-        # )
-        self._second_try_submissions(service, course, coursework)
+        self._download_submissions(service, course, coursework)
 
     def _get_coursework_by_course_and_code(self, service, course, coursework_code):
         courseworks = (
@@ -43,35 +40,7 @@ class ClassroomFetcher:
                 return cw
         print("CourseWork not Found")
 
-    def _download_student_submissions(
-        self, service, course_student_ids, course, coursework
-    ):
-        counter = 0
-        # bekas_id = "109915369104227089235"
-        for student_id in course_student_ids:
-            # if student_id != bekas_id:
-            #     continue
-            print(counter)
-            counter += 1
-            response = (
-                service.courses()
-                .courseWork()
-                .studentSubmissions()
-                .list(
-                    courseId=course["id"],
-                    courseWorkId=coursework["id"],
-                    userId=student_id,
-                )
-                .execute()
-            )
-            submissions = response.get("studentSubmissions", [])
-            latest_submission = submissions[0]
-            if len(latest_submission["assignmentSubmission"]) == 0:
-                print("not submitted")
-            if len(submissions) != 1:
-                print(submissions)
-
-    def _get_course_student_ids(self, service, course) -> List[str]:
+    def _get_course_students(self, service, course) -> List[str]:
         course_id = course["id"]
         all_students = []
         page_token = None
@@ -86,8 +55,7 @@ class ClassroomFetcher:
             if "nextPageToken" not in students or students["nextPageToken"] is None:
                 break
             page_token = students["nextPageToken"]
-        student_ids = [student["userId"] for student in all_students]
-        return student_ids
+        return all_students
 
     def _get_course_by_code(self, service, course_code: str) -> str:
         try:
@@ -130,7 +98,7 @@ class ClassroomFetcher:
                 token.write(creds.to_json())
         return creds
 
-    def _second_try_submissions(self, service, course, coursework):
+    def _download_submissions(self, service, course, coursework):
         submissions = (
             service.courses()
             .courseWork()
@@ -139,29 +107,29 @@ class ClassroomFetcher:
             .execute()
             .get("studentSubmissions", [])
         )
+        download_folder = os.path.join(
+            "C:/Users/Koba.Paikidze/Desktop/Books/nand2tetris/hardware_tester/download_folder"
+        )
+        download_folder = os.path.join(download_folder, course["id"], coursework["id"])
+        _ = os.path.dirname(download_folder)
+        os.makedirs(_, exist_ok=True)
 
-        # Create a folder to store the downloaded files
-        # download_folder = "submissions"
-        # os.makedirs(download_folder, exist_ok=True)
-
-        # Download the files for each submitted student
         for submission in submissions:
             if (
                 submission["state"] == "TURNED_IN"
                 and len(submission["assignmentSubmission"]) > 0
             ):
                 user_id = submission["userId"]
-                print(user_id)
-                # attachments = submission["assignmentSubmission"].get("attachments", [])
-                # for attachment in attachments:
-                #     file_link = attachment["link"]["url"]
-                #     file_name = attachment["driveFile"]["title"]
-                #     file_path = os.path.join(download_folder, file_name)
-                #
-                #     Download the file
-                # file_data = service._http.request(file_link, method="GET")[1]
-                # with open(file_path, "wb") as file:
-                #     file.write(file_data)
-                # print(f"Downloaded file: {file_name}")
+                attachments = submission["assignmentSubmission"].get("attachments", [])
+                for attachment in attachments:
+                    file_link = attachment["driveFile"]["alternateLink"]
+                    file_name = attachment["driveFile"]["title"]
+                    file_path = os.path.join(download_folder, file_name)
+                _ = os.path.dirname(file_path)
+                os.makedirs(_, exist_ok=True)
+                file_data = service._http.request(file_link, method="GET")[1]
+                with open(file_path, "wb") as file:
+                    file.write(file_data)
+                print(f"Downloaded file: {file_name}")
             else:
                 print("not submitted")
--- n2tconfig.py
+++ n2tconfig.py
@@ -5,3 +5,4 @@ N2T_WORK_AREA_PATH: str = os.getenv("n2t_work_area_path")  # type: ignore
 TEST_SUCCESS = "End of script - Comparison ended successfully"
 GOOGLE_API_CREDENTIALS: str = os.getenv("google_api_credentials")  # type: ignore
 GOOGLE_API_TOKENS_PATH: str = os.getenv("google_api_tokens_path")  # type: ignore
+DOWNLOAD_FOLDER: str = os.getenv("n2t_homework_files_download_folder")
