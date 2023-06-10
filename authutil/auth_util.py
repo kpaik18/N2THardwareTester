84b0afa6c771670c33e874cb310f985bd240dc9a
Koba.Paikidze <koba.paikidze@ol.ge>
2023-06-10 13:04:33 +0400

    removed google auth methods from separate classes, refactored it into google auth util

---
+++ authutil/auth_util.py
@@ -0,0 +1,32 @@
+import os
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+from n2tconfig import GOOGLE_API_CREDENTIALS, GOOGLE_API_TOKENS_PATH
+
+
+def auth_on_google_classroom():
+    credentials_path = GOOGLE_API_CREDENTIALS
+    tokens_path = GOOGLE_API_TOKENS_PATH
+    SCOPES = [
+        "https://www.googleapis.com/auth/classroom.courses.readonly",
+        "https://www.googleapis.com/auth/classroom.coursework.students",
+        "https://www.googleapis.com/auth/classroom.rosters",
+        "https://www.googleapis.com/auth/drive.readonly",
+    ]
+    creds = None
+    if os.path.exists(tokens_path):
+        creds = Credentials.from_authorized_user_file(tokens_path, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(tokens_path, "w") as token:
+            token.write(creds.to_json())
+    return creds
--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -2,14 +2,12 @@ import os.path
 from dataclasses import dataclass
 from typing import List, Protocol
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 
-from n2tconfig import DOWNLOAD_FOLDER, GOOGLE_API_CREDENTIALS, GOOGLE_API_TOKENS_PATH
+from authutil.auth_util import auth_on_google_classroom
+from n2tconfig import DOWNLOAD_FOLDER
 
 
 @dataclass
@@ -25,7 +23,7 @@ class IHomeworkFetcher(Protocol):
 
 class ClassroomFetcher:
     def get_assignment_submissions(self, course_code: str, coursework_code: str):
-        creds = self._auth()
+        creds = auth_on_google_classroom()
         service = build("classroom", "v1", credentials=creds)
         course = self._get_course_by_code(service, course_code)
         course_students = self._get_course_students(service, course)
@@ -83,31 +81,6 @@ class ClassroomFetcher:
 
         except HttpError as error:
             print("An error occurred: %s" % error)
-
-    def _auth(self) -> Credentials:
-        credentials_path = GOOGLE_API_CREDENTIALS
-        tokens_path = GOOGLE_API_TOKENS_PATH
-        SCOPES = [
-            "https://www.googleapis.com/auth/classroom.courses.readonly",
-            "https://www.googleapis.com/auth/classroom.coursework.students",
-            "https://www.googleapis.com/auth/classroom.rosters",
-            "https://www.googleapis.com/auth/drive.readonly",
-        ]
-        creds = None
-        if os.path.exists(tokens_path):
-            creds = Credentials.from_authorized_user_file(tokens_path, SCOPES)
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    credentials_path, SCOPES
-                )
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open(tokens_path, "w") as token:
-                token.write(creds.to_json())
-        return creds
 
     def _download_submissions(self, service, course, coursework):
         submissions = (
