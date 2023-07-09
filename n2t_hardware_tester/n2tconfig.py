17b8d2ba6b457572a1deb2964fa08e8c11f16e83
Kobbit <kpaik18@freeuni.edu.ge>
2023-07-09 15:59:58 +0400

    confiug update

--- n2t_hardware_tester/n2tconfig.py
+++ n2t_hardware_tester/n2tconfig.py
@@ -3,6 +3,7 @@ import os
 PROJECT_PATH: str = os.getenv("n2t_hardware_tester_project_path")  # type: ignore
 N2T_WORK_AREA_PATH: str = os.getenv("n2t_work_area_path")  # type: ignore
 TEST_SUCCESS = "End of script - Comparison ended successfully"
+TIMESTAMP_NOT_FOUND_VALUE_LATE_DAYS = 99999
 GOOGLE_API_CREDENTIALS: str = os.getenv("n2t_google_api_credentials")  # type: ignore
 GOOGLE_API_TOKENS_PATH: str = os.getenv("n2t_google_api_tokens_path")  # type: ignore
 DOWNLOAD_FOLDER: str = os.getenv("n2t_homework_files_download_folder")  # type: ignore
