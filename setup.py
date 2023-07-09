2d463f6a828ea0b4d104fadd4abbe95c00671cc4
Kobbit <kpaik18@freeuni.edu.ge>
2023-07-09 14:12:50 +0400

    update setup release version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="n2t-hardware-tester-test1",
-    version="1.0.2",
+    name="n2t-hardware-tester",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=[
         "pytest",
